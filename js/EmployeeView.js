d520cdd306d3f34690ffa37fd6f83f4a198d4811
gatt <gatt@gatt.com>
2013-11-10 15:42:07 +0100

    a

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -3,7 +3,7 @@ var EmployeeView = function(employee) {
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
-        this.el.on('click', '.add-contact-btn', this.addToContacts);
+        this.el.on(‘click’, ‘.add-location-btn’, function(){that.addLocation(event);});
         this.el.on('click', '.change-pic-btn', this.changePicture);
     };
 
