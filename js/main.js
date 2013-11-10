9bbb5d0193b9beca41b798fa42c195a9e17bccc2
gatt <gatt@gatt.com>
2013-11-10 12:58:37 +0100

    s

--- js/main.js
+++ js/main.js
@@ -21,9 +21,9 @@ var app = {
 			$('body').on('touchstart', 'a', function(event) {
 				$(event.target).addClass('tappable-active');
 			});
-			$('body').on('touchend', 'a', function(event) {
+			/* $('body').on('touchend', 'a', function(event) {
 				$(event.target).removeClass('tappable-active');
-			});
+			}); */
 		} else {
 			// ... if not: register mouse events instead
 			$('body').on('mousedown', 'a', function(event) {
